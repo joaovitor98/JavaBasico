bde2ea2a26403b6d39736b0a9e4f415edd6e22be
João Vitor Brandão da Silva <47721066+joaovitor98@users.noreply.github.com>
2025-05-20 23:20:15 -0300

    Atualização de código

--- src/MatrizesVetores/Aula2.java
+++ src/MatrizesVetores/Aula2.java
@@ -1,10 +1,23 @@
 package MatrizesVetores;
 
+import java.util.Scanner;
+
 public class Aula2 {
     public static void main(String[] args) {
-        int vetor[] = new int[10];
-        for (int i : vetor) {
-            System.out.println(vetor[i]++);
+        Scanner leitura = new Scanner(System.in);
+        int n = leitura.nextInt();
+        double vetor[] = new double[n];
+        for (int i= 0; i<vetor.length;i++) {
+            System.out.println("Digite um numero");
+            vetor[i] = leitura.nextDouble();
         }
+        double soma = 0.0;
+
+        for (int i=0; i< vetor.length; i++){
+            soma += vetor[i];
+        }
+        double media = soma/n;
+
+        System.out.printf("Media de Altura = %.2f", media);
     }
 }
